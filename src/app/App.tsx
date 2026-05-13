34e2340514f1d1f17b236fe03d3482849f6b4bb4
nabilrn <2211522018_nabil@student.unand.ac.id>
2026-05-13 12:37:45 +0700

    fix github pages issuse

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { HashRouter, Routes, Route } from 'react-router';
 import { LandingPage } from '../components/LandingPage';
 import { GlobeExperience } from '../components/GlobeExperience';
 import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/explore" element={<GlobeExperience />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
